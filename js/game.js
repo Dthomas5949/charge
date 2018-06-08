b6224d3c3265da13d38748da8f5411b5a8be9934
Durias Thomas <Duriasthomas@Yahoo.com>
2018-06-07 17:16:31 -0700

    Update game.js
    Added in enemy name randomization. Added a restart button for the user to select.

--- js/game.js
+++ js/game.js
@@ -18,7 +18,7 @@ var username = 'UnknownHero'; // Changed default username
 var CPULives = 3;
 var turns = 0;
 var cpumove = moves[Math.floor((Math.random() * 3) + 0)];
-var foe = 'Procrastination';
+var foe = generateEnemyName();
 var blockingsize = '230';
 var attackingsize = '230';
 var chargingsize = '130';
@@ -45,6 +45,23 @@ function start() {
   $("#startbtn").css("display", "none");
   $("#foe").append("<h5 id='rightlivesheader' class='headers'>Health:</h5><div class='progress' id='lives'><div class='progress-bar' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%' id='rightbar'></div></div><h5 id='rightchargeheader' class='headers'>Charge:</h5><div class='progress' id='charge'><div class='progress-bar' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width: 0%' id='chargerightbar'></div></div>");
   $("#user").append("<h5 id='leftlivesheader' class='headers'>Health:</h5><div class='progress' id='lives'><div class='progress-bar' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%' id='leftbar'></div></div><h5 id='leftchargeheader' class='headers'>Charge:</h5><div class='progress' id='charge'><div class='progress-bar' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width: 0%' id='chargeleftbar'></div></div>");
+    
+}
+
+function generateEnemyName() {
+    var a = ["xX", "xx"];
+    var b = ["Dara", "Barrett", "Patience", "Dalton", "Quincy", "Davis", "Jack", "Oren", "Xander", "Yoshio", "Zia", "Jeanette", "Lionel", "Griffin", "Pascale", "Casey", "Jacqueline", "Charde",
+        "Vance", "Ezra", "Sarah", "Reece", "Barbara", "Aaron", "Zelenia", "Kimberley", "Imogene", "Casey", "Reese", "Shannon", "Drake", "Ferris", "Malachi", "Nissim", "Odessa", "Giselle",
+        "Brent", "Tarik", "Zena", "Herman", "Ria", "Aileen", "Raya", "Logan", "Brody", "Prescott", "Donovan", "Gregory", "Eaton", "Leroy", "Alexis", "Paul", "Keely", "Thaddeus",
+        "Shannon", "Dane", "Price", "Charde", "Gabriel", "Sarah", "Jemima", "Ian", "Arando", "Thaddeus", "Paki", "Dylan", "Aline", "Malachi", "Buffy", "Callum", "Hollee", "Lionel",
+        "Autumn", "Cody", "Janna", "Nathaniel", "Giselle", "Christopher", "Quinn", "Gabriel", "Mohammad", "Dieter", "Chandler", "Juliet", "Keely", "Charlotte", "Callum", "Ira", "Darrel",
+        "Gavin", "Rahim", "Voldemort", "Buckminster", "Demetria", "Procrastination", "Nevada", "Hammett", "Hayley", "Jana", "Zahir"];
+    var c = ["Xx", "xx"];
+    var rA = Math.floor(Math.random() * a.length);
+    var rB = Math.floor(Math.random() * b.length);
+    var rC = Math.floor(Math.random() * c.length);
+    var name = a[rA] + b[rB] + c[rC];
+    return name;
 }
 
 function beginAgain() {
@@ -54,7 +71,7 @@ function beginAgain() {
 function gui(){
   $("#leftplayer").attr("src", "img/stay.PNG").attr("width", staysize);
   $("#rightplayer").attr("src", "img/stay.PNG").attr("width", staysize);
-  $(".gui").append("<div id='movepick'><h3>What move are you going to use?<br></h3><button onclick='charge();'><img src='img/electric.png' class='icon'></button><button onclick='attack();'><img src='img/dualsword.png' class='icon'></button><button onclick='block();'><img src='img/shield.png' class='icon'>");
+  $(".gui").append("<div id='movepick'><h3>What move are you going to use?<br></h3><button onclick='charge();'><img src='img/electric.png' class='icon'></button><button onclick='attack();'><img src='img/dualsword.png' class='icon'></button><button onclick='block();'><img src='img/shield.png' class='icon'></button><br><button class='endgame' onclick='endgame();'>End Game</button></br><br><button class='restartGame' onclick='restartGame();'>Restart Game</button></br></div>");
   $(".headers").css("display", "block");
   
 }
@@ -82,6 +99,18 @@ function block() {
 function endgame() {
   usermove = "stop";
   resumeGame();
+}
+
+function restartGame() {
+    $("#comment").html(" ");
+    if (userLives == 0 || CPULives == 0) {
+        start();
+    }
+    userLives = 3;
+    CPULives = 3;
+    userCharge = 0;
+    CPUCharge = 0;
+    turnWrapup();
 }
 
 function resumeGame(){
@@ -183,7 +212,7 @@ function resumeGame(){
              $(".endarea").css("visibility", "visible");
         } else if(CPULives < 1){
              $("h4,table,.end").remove();
-             $("#playarea").append("<div class='endtext' id='enduser'><h1>"+ username + " Wins!</h1></div>");
+             $("#playarea").append("<div class='endtext' id='enduser'><h1><b>"+ username + " Wins!</b></h1></div>");
              setTimeout(userWin, waittime);
              $("#rightplayer").attr("src", "").attr("width", "0");
              $("#rightplayer").attr("src", "img/dead.PNG").attr("width", "200");
